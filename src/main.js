93298061693a54ced0287d64f77305a9bd5232cb
Leo Hughes <zhihy123@qq.com>
2016-11-05 17:08:21 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import VueRouter from 'vue-router'
 import App from './App'
 import Add from './components/Add/Add'
 
-
+// 声明使用vue-router
 Vue.use(VueRouter)
 
+// 创建路由表
 const routes = [
   {
     path: '/',
@@ -17,11 +18,12 @@ const routes = [
   }
 ]
 
+// 注册路由表
 const router = new VueRouter({
   routes
 })
 
-
+// 挂载实例，启动应用
 const app = new Vue({
   el: '#app',
   router
