876532867011db3f5ce88d19262bb84a8225f790
Leo Hughes <zhihy123@qq.com>
2016-11-22 15:14:40 +0800

    update

--- src/components/Add/Add.vue
+++ src/components/Add/Add.vue
@@ -1,10 +1,7 @@
 <template lang="pug">
   section#add
     Head_(:title="name")
-    form.pure-form.pure-g
-      label.pure-u-1.pure-u-sm-1.pure-u-md-1-4.pure-u-lg-1-4 Words:
-      .pure-u-1.pure-u-sm-1.pure-u-md-3-4.pure-u-lg-3-4
-        input.pure-input-1(type="text" placeholder="Enter the new words.(20)" maxlength="20" required v-model="word")
+
 
 </template>
 
